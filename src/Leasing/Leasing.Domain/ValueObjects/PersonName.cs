7773f855b79b5efe2728e8bf4444a9c226655115
joshua <velascojoshua916@gmail.com>
2025-08-21 18:31:35 +0800

    PersonName

---
+++ src/Leasing/Leasing.Domain/ValueObjects/PersonName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leasing.Domain.ValueObjects
+{
+    public record PersonName(string FirstName, string LastName)
+    {
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
